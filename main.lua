15acc28246b3df1a6a80332ba7dc8a5d0ca4a82a
helloxyzcodervisuals <2022997467@qq.com>
2026-02-12 10:50:04 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 repeat task.wait() until game:IsLoaded()
---abc
+--abc.gg
 local function isAdonisAC(tab) 
     return rawget(tab,"Detected") and typeof(rawget(tab,"Detected"))=="function" and rawget(tab,"RLocked") 
 end
